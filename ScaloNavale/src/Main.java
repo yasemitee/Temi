f1c3d07229099bbb9f825eb3fe0a635408e6b9f0
yasemitee <116098124+yasemitee@users.noreply.github.com>
2023-03-04 10:31:02 +0100

    Update Main.java

--- ScaloNavale/src/Main.java
+++ ScaloNavale/src/Main.java
@@ -10,29 +10,33 @@ public class Main {
         int partenza = Integer.parseInt(args[1]);
         int arrivo = Integer.parseInt(args[2]);
         File file = new File("input.txt");
-        Scanner sc = new Scanner(file);
-        int nMoli = sc.nextInt();
-        ScaloNavale scalo = new ScaloNavale(nMoli);
-        sc.nextLine();
-        for (int i = 0; i < nMoli; i++) {
-            String[] input = sc.nextLine().split(" ");
-            List<String> nomiNavi = new ArrayList<>();
-            List<Integer> pesiNavi = new ArrayList<>();
-            for (int j = 0; j < input.length; j++) {
-                if (j % 2 == 0) {
-                    nomiNavi.add(input[j]);
-                } else {
-                    pesiNavi.add(Integer.parseInt(input[j]));
+        try (Scanner sc = new Scanner(file)) {
+            int nMoli = sc.nextInt();
+            ScaloNavale scalo = new ScaloNavale(nMoli);
+            sc.nextLine();
+            for (int i = 0; i < nMoli; i++) {
+                String[] input = sc.nextLine().split(" ");
+                List<String> nomiNavi = new ArrayList<>();
+                List<Integer> pesiNavi = new ArrayList<>();
+                for (int j = 0; j < input.length; j++) {
+                    if (j % 2 == 0) {
+                        nomiNavi.add(input[j]);
+                    } else {
+                        pesiNavi.add(Integer.parseInt(input[j]));
+                    }
+                }
+                for (int j = 0; j < nomiNavi.size(); j++) {
+                    var nave = new Molo.NaveCargo(nomiNavi.get(j), pesiNavi.get(j));
+                    scalo.attraccaNaveA(nave, i);
                 }
             }
-            for (int j = 0; j < nomiNavi.size(); j++) {
-                var nave = new Molo.NaveCargo(nomiNavi.get(j), pesiNavi.get(j));
-                scalo.attraccaNaveA(nave, i);
-            }
+            System.out.print(scalo);
+            scalo.spostamento(nSpostamenti, partenza, arrivo);
+            System.out.println(nSpostamenti);
+            System.out.println(scalo);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        System.out.print(scalo);
-        scalo.spostamento(nSpostamenti,partenza,arrivo);
-        System.out.println(nSpostamenti);
-        System.out.println(scalo);
     }
 }
